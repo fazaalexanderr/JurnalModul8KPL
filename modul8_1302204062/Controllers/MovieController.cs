0c5473c9e30e4913feb6a196cc0e0f4726159faf
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-23 11:25:09 +0700

    menambahkan api

---
+++ modul8_1302204062/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302204062.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ modul8_1302204062/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace modul8_1302204062
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string[] Stars { get; set;}
+        public string Description { get; set; }
+
+        public Movie()
+        {
+
+        }
+
+    }
+}
